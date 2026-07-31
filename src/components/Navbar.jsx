a02793ec5446689cd947fe925ac55f40ca34535f
Saniya Gupta <saniya.gupta@gammaedge.io>
2026-07-31 12:56:59 +0530

    Fixed ui 4.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,10 @@ const LINKS = [
 export default function Navbar() {
   return (
     <header className="navbar">
-      <a href="#home" className="navbar-brand">Satyam Foundation School</a>
+      <a href="#home" className="navbar-brand">
+        <span className="navbar-logo">SFS</span>
+        Satyam Foundation School
+      </a>
       <nav className="navbar-links">
         {LINKS.map(([label, href]) => (
           <a key={href} href={href}>{label}</a>
